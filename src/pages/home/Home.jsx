cc09d02185973e88719f33a6fda88ccfd0eb77ca
pratik-71 <pratikdabhade66344@gmail.com>
2024-02-11 12:32:13 +0530

    UI/UX DONE

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -11,6 +11,9 @@ import "../../styles/Home_style.css"
 import Section2 from './Section2'
 import Section3 from './Section3'
 import Section4 from './Section4'
+import Section5 from './Section5'
+import Section6 from './Section6'
+import Section7 from './Section7'
 
 const Home = () => {
 
@@ -29,6 +32,15 @@ const Home = () => {
 
       {/* Home promotion Section 4 */}
       <Section4/>
+
+      {/* Shop Section 5 */}
+      <Section5/>
+
+      {/* Blog section 6 */}
+      <Section6/>
+
+      {/* COntact section 7 */}
+      <Section7/>
       
     </Layout>
     </>
